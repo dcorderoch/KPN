07e6b1f6a8d95a13c58fffa971c1c5344b37fddc
David Cordero Chavarría <dcorderoch@ieee.org>
2021-05-09 19:28:54 -0600

    make it simpler, and make it stop at the end

--- kahn.py
+++ kahn.py
@@ -5,92 +5,39 @@ an attempt to make Kahn Process networks
 from threading import Thread
 from queue import Queue
 
-import time # to simulate blocking
-
-class KahnThread(Thread):
-    """
-    class to simulate a node, it runs the node (lambda function) that is passed to it
-    """
-    def __init__(self, fn):
-        """
-        class initialization dunder method
-        """
+class KPNThread(Thread):
+    def __init__(self, *, fn, name, i_q, o_q):
         super().__init__()
         self.fn = fn
-        self.in_queues = []
-        self.out_queues = []
-
-    def __setitem__(self, index, q):
-        """
-        class set dunder method
-        """
-        self.out_queues[index] = q
-
-    def __getitem__(self, index):
-        """
-        class get dunder method
-        """
-        return self.in_queues[index]
+        self.name = name
+        self.i_queues = i_q
+        self.o_queues = o_q
 
     def run(self):
-        """
-        Thread loop
-        """
         while True:
-            # get the inputs in the queue
-            inputs = [q.get(True) for q in self.in_queues]
+            # tuple of tuples
+            inputs = [q.get(True) for q in self.i_queues]
+            _next, _sum, able = self.fn(inputs[0])
+            if able:
+                for q in self.o_queues:
+                    q.put((_next, _sum, able))
+            else:
+                print(f'result of {self.name}: {_sum}')
+                return
 
-            # simulate blocking # TODO: check if necessary, Queue.get() should block
-            # time.sleep(0.25)
-
-            # generate output of function
-            results = self.fn(*inputs)
-
-            # put output in the output queue
-            if isinstance(results, tuple):
-                for result, q in zip(results, self.out_queues):
-                    q.put(result)
-            elif len(self.out_queues):
-                self.out_queues[0].put(results)
-
-def connect(*connections):
-    """
-    make the producer-consumer connection between two blocks
-    """
-    processes = {}
-    for producer, consumer in connections:
-        if not producer in processes:
-            processes[producer] = KahnThread(producer)
-        if not consumer in processes:
-            processes[consumer] = KahnThread(consumer)
-
-        q = Queue()
-        processes[producer].out_queues.append(q)
-        processes[consumer].in_queues.append(q)
-
-    for process in processes.values():
-        process.start()
-
-
-def printer(filename):
-    """
-    generate a function that will print to stdouot, and append to filename
-    """
-    return lambda content: (print(content), open(filename, 'a').write(str(content) + '\n'))
-
-# generate the initial value to pass to factorial
-seed = int(input('Number: '))
-
-def update_seed(x):
-    seed = x
-
-# run if this is the program called, but not if it's imported from another
 if __name__ == '__main__':
-    # "generate" seed
-    b = lambda: seed
-    # generate a function that calcultes the factorial
-    fact_aux = lambda x: 1 if x == 0 else x * fact_aux(x - 1)
-    # generate a function that returns an argument to itself
-    fact = lambda x: x
+    fac = lambda t: (t[0] - 1, t[0] * t[1], t[0] > 1)
 
-    connect((b, fact), (fact, fact_aux), (fact_aux, printer('test.txt')), (fact_aux, lambda x: update_seed(x)))
+    seed_no = int(input('initial number: '))
+
+    print("")
+
+    a_q = Queue()
+    # (seed number, multiplicative identity for factorial, still going)
+    # once "still going" is false, calculation is done, and thread will stop
+    seed = (seed_no, 1, True)
+    a_q.put(seed)
+
+    factorializer = KPNThread(fn=fac, name="factorial", i_q=(a_q,), o_q=(a_q,))
+
+    factorializer.start()
